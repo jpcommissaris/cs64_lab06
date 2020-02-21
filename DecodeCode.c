e87c7d2c223e3a2e9750de87702802c1c1041a9f
Julian Commissaris <jpcommissaris@gmail.com>
2020-02-20 20:48:49 -0800

    passes tests

--- DecodeCode.c
+++ DecodeCode.c
@@ -1,17 +1,19 @@
 #include "DecodeCode.h"
 
+// Julian Commissaris && Rick Han 
+
 
 mipsinstruction decode(int value)
 {
 	mipsinstruction instr;
 
 	// TODO: fill in the fields
-	instr.funct = 15;
-	instr.immediate = -10;
-	instr.rd = 0;
-	instr.rt = 5;
-	instr.rs = 3;
-	instr.opcode = 25;
+	instr.funct = value & 0x3F;
+	instr.immediate = ((value & 0xFFFF) << 16) >> 16;
+	instr.rd = (value & 0x1F) >> 11;
+	instr.rt = (value & 0x1F) >> 16;
+	instr.rs = (value & 0x1F) >> 21;
+	instr.opcode = (value & 0x2F) >> 26;
 
 	return instr;
 }
